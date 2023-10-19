8ecbea40a418b1c9b039378e2f2596aa95b2bffe
Melonorm <shvagerv@gmail.com>
2023-10-19 16:05:59 +0300

    обвязка typeorm, начальная настройка main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ConfigModule} from "@nestjs/config";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {getTypeOrmConfig} from "./configs/typeorm.config";
 
 @Module({
   imports: [
-      ConfigModule.forRoot({
-          isGlobal: true,
-          expandVariables: true
-      }),
-      TypeOrmModule.forRoot({
-
+      ConfigModule.forRoot(),
+      TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: getTypeOrmConfig
       })
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
+  const PORT = process.env.HTTP_PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe())  // пайпы
+  await app.listen(
+      PORT,
+      () => console.log(`Server started at ${PORT} port...`));
 }
 bootstrap();
